43515b3154f8950e59263d7f89cb070bb950c289
Naomi Carrigan <nhcarrigan@gmail.com>
2023-11-11 13:41:46 -0800

    feat: build initial prototype

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,71 @@
-import { logHandler } from "./utils/logHandler";
+import { Client, Events, GatewayIntentBits, WebhookClient } from "discord.js";
 
-/**
- * The linter will expect JSDoc declarations for all exported functions.
- *
- * @param {string} name Variables should be typed, and full sentences are expected.
- * @returns {string} The return type should be specified.
- */
-const main = (name: string): string => {
-  const string = `Hello ${name}!`;
-  logHandler.log("info", string);
-  return string;
-};
+(async () => {
+  const debug = new WebhookClient({
+    url: process.env.DEBUG || "",
+  });
+  const bot = new Client({
+    intents: [
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.MessageContent,
+    ],
+  });
 
-main("Naomi");
+  bot.on(Events.ClientReady, async () => {
+    await debug.send("Bot is online and ready to help!");
+  });
 
-export default main;
+  bot.on(Events.MessageCreate, async (message) => {
+    if (message.author.id !== "1172850885571911760") {
+      return;
+    }
+    await message.react("✅").catch(async (err) => {
+      await debug.send({
+        content: `[Failed to add reaction:](<${message.url}>) ${err.message}`,
+      });
+    });
+  });
+
+  bot.on(Events.MessageReactionAdd, async (r, user) => {
+    if (user.bot) {
+      return;
+    }
+    const reaction = await r.fetch().catch(async (err) => {
+      await debug.send({
+        content: `[Failed to fetch reactions:](<${r.message.url}>) ${err.message}`,
+      });
+      return null;
+    });
+    if (!reaction) {
+      return;
+    }
+    if (reaction.count > 2) {
+      await r.users.remove(user.id);
+      return;
+    }
+    const message = await r.message.fetch().catch(async (err) => {
+      await debug.send({
+        content: `[Failed to fetch message:](<${r.message.url}>) ${err.message}`,
+      });
+      return null;
+    });
+    if (!message) {
+      return;
+    }
+    const files = [...message.attachments.values()];
+    await user
+      .send({
+        content: `[Here is the donation commission you claimed~!](${message.url})\n> ${message.content}`,
+        files,
+      })
+      .catch(async (err) => {
+        await debug.send({
+          content: `[Failed to DM ${user.username}:](<${r.message.url}>) ${err.message}`,
+        });
+      });
+  });
+
+  await bot.login(process.env.TOKEN);
+})();
