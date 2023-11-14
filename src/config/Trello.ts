4d8df6d7b68699be2e1234af2b1c5f59ae740e5a
Naomi Carrigan <nhcarrigan@gmail.com>
2023-11-13 20:35:45 -0800

    feat: implement first round of trello integration

---
+++ src/config/Trello.ts
@@ -0,0 +1,27 @@
+export const Trello = {
+  boardId: "",
+  newCardListId: "6552f24264ac58e98f8b78cd",
+  platformLabels: {
+    discord: "6552f2b18af54bcca998abfe",
+    twitter: "6552f2cb9f21432b29106558",
+    email: "6552f2cee1258dfe159d48c8",
+  },
+  actionLabels: {
+    donate: "6552f2d257c42c85759d0199",
+    call: "6552f2d6d0e69a4c17f5f8ab",
+    protest: "6552f2dbfa9f5ac4267b136a",
+  },
+  checklistId: "6552f66d91daf000137ed4d2",
+};
+
+export const PlatformsToLabel: Record<string, string> = {
+  Email: Trello.platformLabels.email,
+  Discord: Trello.platformLabels.discord,
+  Twitter: Trello.platformLabels.twitter,
+};
+
+export const ActionsToLabel: Record<string, string> = {
+  "Sent a donation": Trello.actionLabels.donate,
+  "Called my representatives": Trello.actionLabels.call,
+  "Attended a protest": Trello.actionLabels.protest,
+};
--- src/server/serve.ts
+++ src/server/serve.ts
@@ -5,6 +5,7 @@ import https from "https";
 import { AttachmentBuilder } from "discord.js";
 import express from "express";
 
+import { ActionsToLabel, PlatformsToLabel, Trello } from "../config/Trello";
 import { ExtendedClient } from "../interface/ExtendedClient";
 
 /**
@@ -30,15 +31,96 @@ export const serve = async (bot: ExtendedClient) => {
         return;
       }
       res.status(200).send("OK~!");
-      const { name, imageUrl } = req.body;
+      const { name, imageUrl, action, platform, handle, note, email } =
+        req.body;
       const image = await fetch(imageUrl);
       const imageBuffer = await image.arrayBuffer();
       const imageFinal = Buffer.from(imageBuffer);
       const file = new AttachmentBuilder(imageFinal, {
         name: "reference.png",
       });
+      if (!process.env.TRELLO_TOKEN || !process.env.TRELLO_KEY) {
+        await bot.debug.send({
+          content:
+            "Cannot create card on Trello. Missing environment variables.",
+        });
+        await bot.comm.send({
+          content: `${name} | Trello failed to generate. | [Reference](<${image.url}>)`,
+          files: [file],
+        });
+        return;
+      }
+      const cardRes = await fetch(
+        `https://api.trello.com/1/cards?idList=${Trello.newCardListId}&key=${
+          process.env.TRELLO_KEY
+        }&token=${process.env.TRELLO_TOKEN}&name=${encodeURIComponent(
+          name
+        )}&desc=${encodeURIComponent(
+          `${name} helped us with: ${action}\n\nPlease contact them at ${
+            platform === "Email" ? email : `${handle} on ${platform}`
+          } to update them on art.\n\nUSER NOTE: ${note}`
+        )}`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+          },
+        }
+      );
+      const card = (await cardRes.json()) as { id: string; url: string };
+      await fetch(
+        `https://api.trello.com/1/cards/${card.id}/attachments?key=${
+          process.env.TRELLO_KEY
+        }&token=${
+          process.env.TRELLO_TOKEN
+        }&name=reference.png&url=${encodeURIComponent(imageUrl)}&setCover=true`,
+        {
+          method: "POST",
+          headers: {
+            accept: "application/json",
+          },
+        }
+      );
+      // Use this if we want to add checklist to every card :3
+      //   await fetch(
+      //     `https://api.trello.com/1/cards/${
+      //       card.id
+      //     }/checklists?idChecklistSource=${
+      //       Trello.checklistId
+      //     }&name=${encodeURIComponent("To Do")}&key=${
+      //       process.env.TRELLO_KEY
+      //     }&token=${process.env.TRELLO_TOKEN}`,
+      //     {
+      //       method: "POST",
+      //       headers: {
+      //         accept: "application/json",
+      //       },
+      //     }
+      //   );
+      if (PlatformsToLabel[platform]) {
+        await fetch(
+          `https://api.trello.com/1/cards/${card.id}/idLabels?value=${PlatformsToLabel[platform]}&key=${process.env.TRELLO_KEY}&token=${process.env.TRELLO_TOKEN}`,
+          {
+            method: "POST",
+            headers: {
+              accept: "application/json",
+            },
+          }
+        );
+      }
+      if (ActionsToLabel[action]) {
+        await fetch(
+          `https://api.trello.com/1/cards/${card.id}/idLabels?value=${ActionsToLabel[action]}&key=${process.env.TRELLO_KEY}&token=${process.env.TRELLO_TOKEN}`,
+          {
+            method: "POST",
+            headers: {
+              accept: "application/json",
+            },
+          }
+        );
+      }
       await bot.comm.send({
-        content: `${name} | Trello coming soon | [Reference](<${image.url}>)`,
+        content: `${name} | [Trello](<${card.url}>) | [Reference](<${image.url}>)`,
         files: [file],
       });
       return;
