aa4c75f89f14493db11c3278e43f3777a96194ef
Naomi Carrigan <nhcarrigan@gmail.com>
2023-11-14 17:38:18 -0800

    feat: add timeout commands

--- src/events/onMessageCreate.ts
+++ src/events/onMessageCreate.ts
@@ -1,6 +1,7 @@
-import { Message } from "discord.js";
+import { Message, PermissionFlagsBits } from "discord.js";
 
 import { ExtendedClient } from "../interface/ExtendedClient";
+import { getMuteDurationUnit } from "../utils/getMuteDurationUnit";
 import { isValidWebhook } from "../utils/isValidWebhook";
 import { logHandler } from "../utils/logHandler";
 
@@ -17,6 +18,74 @@ export const onMessageCreate = async (
 ) => {
   try {
     if (!message.author.bot) {
+      if (
+        message.member?.permissions.has(PermissionFlagsBits.ModerateMembers)
+      ) {
+        if (message.content.startsWith("!mute")) {
+          const [, id, durationString, ...reason] =
+            message.content.split(/\s+/);
+          const target = await message.guild?.members.fetch(id);
+          if (!target) {
+            await message.reply("Target not found.");
+            return;
+          }
+          const durationNumber = parseInt(
+            durationString.replace(/\w$/, ""),
+            10
+          );
+          const durationUnit = durationString.replace(/^\d+/, "");
+          const conversion = getMuteDurationUnit(durationUnit);
+          if (!conversion) {
+            await message.reply(`Invalid unit ${durationUnit}.`);
+            return;
+          }
+          const time = durationNumber * conversion;
+          if (time > 1000 * 60 * 60 * 24 * 28) {
+            await message.reply("Cannot time out user for more than 28 days.");
+            return;
+          }
+          await target.timeout(time, reason.join(" ") || "No reason provided.");
+          await message.reply({ content: "Done" });
+          const logChannel = message.guild?.channels.cache.find(
+            (c) => c.name === "bot-logs"
+          );
+          if (logChannel && "send" in logChannel) {
+            await logChannel?.send({
+              content: `<@!${message.author.id}> muted <@!${target.id}> (${
+                target.id
+              }) - ${durationString} for: ${
+                reason.join(" ") ?? "No reason provided."
+              }`,
+              allowedMentions: {
+                users: [],
+              },
+            });
+          }
+        }
+        if (message.content.startsWith("!unmute")) {
+          const [, id, ...reason] = message.content.split(/\s+/);
+          const target = await message.guild?.members.fetch(id);
+          if (!target) {
+            await message.reply("Target not found.");
+            return;
+          }
+          await target.timeout(null, reason.join(" ") || "No reason provided.");
+          const logChannel = message.guild?.channels.cache.find(
+            (c) => c.name === "bot-logs"
+          );
+          await message.reply({ content: "Done" });
+          if (logChannel && "send" in logChannel) {
+            await logChannel?.send({
+              content: `<@!${message.author.id}> unmuted <@!${target.id}> (${
+                target.id
+              }) for: ${reason.join(" ") ?? "No reason provided."}`,
+              allowedMentions: {
+                users: [],
+              },
+            });
+          }
+        }
+      }
       return;
     }
     if (message.author.bot && !isValidWebhook(message.author.id)) {
